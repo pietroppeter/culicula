22e51909543925d80f16893bf3b30a5f9a112c97
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-10-05 13:55:40 +0000

    img_float ok

--- Hello.py
+++ Hello.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 from sklearn.datasets import load_sample_image
 
 def run():
@@ -7,11 +8,14 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Culìculà! 👋")
+    st.write("# Welcome to :red[C]:gray[u]:green[l]:blue[ì] a color clustering app! 🎨")
 
     which_img = st.selectbox("Pick an image", options=["china.jpg", "flower.jpg"])
     img = load_sample_image(which_img)
-    st.image(img)
+    # st.write(img)
+    # st.image(img)
+    img_float = np.array(img, dtype=np.float64) / 255
+    st.image(img_float) # works the same as img!
 
 
 if __name__ == "__main__":
